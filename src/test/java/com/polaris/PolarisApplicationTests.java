caed246ad34318c5d5691599afc7b5649f5e47a7
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-03 18:32:12 -0300

    Base do projeto

---
+++ src/test/java/com/polaris/PolarisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
